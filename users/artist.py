924240abb2ba280366ace4febf65a55e5cc99586
arty <shachaf.smith@gmail.com>
2022-02-09 10:28:47 +0200

    created artist class

--- users/artist.py
+++ users/artist.py
@@ -2,8 +2,9 @@ from users.user import User, Status
 
 
 class Artist(User):
-    def __init__(self, name, logger):
+    def __init__(self, name, artistID, albums, logger):
         super().__init__(name, Status.premium, logger)
         self.name = name
+        self.artistID = artistID
         self.status = Status.premium
-
+        self.albums = albums
