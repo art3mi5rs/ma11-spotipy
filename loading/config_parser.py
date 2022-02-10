135730663facfe076c7c683fd240bc6a0214c788
arty <shachaf.smith@gmail.com>
2022-02-10 12:23:31 +0200

    created config file parser

---
+++ loading/config_parser.py
@@ -0,0 +1,52 @@
+from configparser import ConfigParser
+
+
+def _init():
+    parser = ConfigParser()
+    parser.read('config.ini')
+    return parser
+
+
+def get_import_path():
+    parser = _init()
+    return parser['files']['import_path']
+
+
+def get_artist_val():
+    parser = _init()
+    return parser['music']['artist_type']
+
+
+def get_album_val():
+    parser = _init()
+    return parser['music']['album_type']
+
+
+def get_song_val():
+    parser = _init()
+    return parser['music']['song_type']
+
+
+def get_free_status_val():
+    parser = _init()
+    return parser['users']['free_status']
+
+
+def get_premium_status_val():
+    parser = _init()
+    return parser['users']['premium_status']
+
+
+def get_playlist_message():
+    parser = _init()
+    return parser['messages']['existing_playlist_message']
+
+
+def get_loader_message():
+    parser = _init()
+    return parser['messages']['json_loader_message']
+
+
+def get_song_identifier():
+    parser = _init()
+    return parser['identifiers']['song_track']
