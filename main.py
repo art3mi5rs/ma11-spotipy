365354f7966f4c99a4107ae8b6001f366c5481ce
arty <shachaf.smith@gmail.com>
2022-02-10 11:20:26 +0200

    attempting to load all files

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from loading.json_loader import JSONLoader
 from music.spotipy_directory import Directory
 from searching.searching import get_artists
 from users.user import User, Status
+from pathlib import Path
 
 
 def main():
     directory = Directory()
     loader = JSONLoader(directory)
     user = User("art3mis", Status().free)
-    loader.load("C:\\Users\\User\\Shachaf-Repos\\Spotipy\\songs\\song_0C9jZPUv4SuaXkuEQw6L40.json")
+    song_imports = 'C:\\Users\\User\\Shachaf-Repos\\Spotipy\\songs'
+    imports = Path(song_imports).glob('*')
+
+    for file in imports:
+        loader.load(file)
+
     print(get_artists(directory.directory, user))
 
 
