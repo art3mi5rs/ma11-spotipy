e6cad406c8b26ea18fc9f14fc12a1885562f9e98
arty <shachaf.smith@gmail.com>
2022-02-10 09:17:32 +0200

    Wrote artist and album searching methods

--- searching/searching.py
+++ searching/searching.py
@@ -3,11 +3,17 @@ from users.artist import Artist
 
 def get_artists(directory, user):
     artists = [artist.name for artist in directory.values]
+    if user.status is "Free":
+        return artists[:5]
     return artists
 
 
 def get_albums(directory, artist_id, user):
-    pass
+    artist_albums = [album.name for album in directory[artist_id].albums]
+    if user.status is "Free":
+        return artist_albums[:5]
+    return artist_albums
+
 
 def get_top_hits(directory, artist_id, user):
     pass
